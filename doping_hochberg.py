4f66ee3e2c8ca1f906493e3e41a2fedada374573
Marc Bolinches <marc@flexcompute.com>
2025-01-30 10:02:00 +0100

    Added inf doping boxes

--- doping_hochberg.py
+++ doping_hochberg.py
@@ -8,35 +8,39 @@ def generate_doping_boxes():
     acceptor_boxes = []
     donors_boxes = []
 
-    acceptor_boxes.append(td.ConstantDoping(box_coords=[[-5, 0, 0],[5, 0.22, 0]], concentration=1e15))
+    acceptor_boxes.append(td.ConstantDoping.from_bounds(rmin=[-5, 0, -td.inf], rmax=[5, 0.22, td.inf], concentration=1e15))
 
     # p implant
-    acceptor_boxes.append(td.GaussianDoping(
-        box_coords=[[-6, -0.3, 0],[-0.15, 0.098, 0]], 
+    acceptor_boxes.append(td.GaussianDoping.from_bounds(
+        rmin=[-6, -0.3, -td.inf],
+        rmax=[-0.15, 0.098, td.inf], 
         concentration=7e17,
         ref_con=1e6,
         width=0.1,
         source="ymax"))
     
     # n implant
-    donors_boxes.append(td.GaussianDoping(
-        box_coords=[[0.15, -0.3, 0],[6, 0.098, 0]], 
+    donors_boxes.append(td.GaussianDoping.from_bounds(
+        rmin=[0.15, -0.3, -td.inf],
+        rmax=[6, 0.098, td.inf], 
         concentration=5e17,
         ref_con=1e6,
         width=0.1,
         source="ymax") )
     
     # p++
-    acceptor_boxes.append(td.GaussianDoping(
-        box_coords=[[-6, -0.3, 0],[-2, 0.22, 0]], 
+    acceptor_boxes.append(td.GaussianDoping.from_bounds(
+        rmin=[-6, -0.3, -td.inf],
+        rmax=[-2, 0.22, td.inf],
         concentration=1e19,
         ref_con=1e6,
         width=0.1,
         source="ymax"))
     
     # n++
-    donors_boxes.append(td.GaussianDoping(
-        box_coords=[[2, -0.3, 0],[6, 0.22, 0]], 
+    donors_boxes.append(td.GaussianDoping.from_bounds(
+        rmin=[2, -0.3, -td.inf],
+        rmax=[6, 0.22, td.inf],
         concentration=1e19,
         ref_con=1e6,
         width=0.1,
@@ -44,16 +48,18 @@ def generate_doping_boxes():
     
     
     # p wg implant
-    acceptor_boxes.append(td.GaussianDoping(
-        box_coords=[[-0.3, 0, 0],[0.06, 0.255, 0]], 
+    acceptor_boxes.append(td.GaussianDoping.from_bounds(
+        rmin=[-0.3, 0, -td.inf],
+        rmax=[0.06, 0.255, td.inf],
         concentration=5e17,
         ref_con=1e6,
         width=0.12,
         source="xmin"))
     
     # n wg implant
-    donors_boxes.append(td.GaussianDoping(
-        box_coords=[[-0.06, 0.02, 0],[0.25, 0.26, 0]], 
+    donors_boxes.append(td.GaussianDoping.from_bounds(
+        rmin=[-0.06, 0.02, -td.inf],
+        rmax=[0.25, 0.26, td.inf],
         concentration=7e17,
         ref_con=1e6,
         width=0.11,
